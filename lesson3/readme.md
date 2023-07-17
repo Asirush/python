aa3c9aa8ea1cc9b22ddf91a527dc9350d4a7abcd
Assir Abdukhalikov <asirushtomblack@gmail.com>
2023-07-17 19:24:52 +0600

    strings in python

---
+++ lesson3/readme.md
@@ -0,0 +1,4 @@
+python -m venv venv ./venv/Scripts/active
+pip install requests
+pip freeze > requerements.txt
+pip install -r requerements.txt
---
+++ lesson3/task_strings.py
@@ -0,0 +1,11 @@
+def qotation_marks() -> None:
+    print('string') # одинарные ковычки
+    print("string") # двойные ковычки
+    print("""string""") # тройные ковычки
+
+    print('book "war and peace"') # экранитрование ковычек
+    print("book 'war and peace'") # экранитрование ковычек
+    
+    return None
+
+qotation_marks()
