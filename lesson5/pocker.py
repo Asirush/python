7d3d8d4d2f25c7b60645acd77d9bdcef579c80a6
Assir Abdukhalikov <asirushtomblack@gmail.com>
2023-07-21 21:48:09 +0600

    finish cards

--- lesson5/pocker.py
+++ lesson5/pocker.py
@@ -21,32 +21,39 @@ CARTS = [
     "Ace"
 ]
 
-player1_wins += {True: 1, False: 0}[player1 > player2]
-player2_wins += {True: 1, False: 0}[player1 < player2]
+# player1_wins += {True: 1, False: 0}[player1 > player2]
+# player2_wins += {True: 1, False: 0}[player1 < player2]
 
-player_win = lambda x, y, str1, str2: str1 if x > y else str2
+# player_win = lambda x, y, str1, str2: str1 if x > y else str2
+        
+def countCardWeigth(value, suit):
+    result = 0
+    result += CARTS.find(value) + 2
+    result *= CART_SUTES.find(suit) + 1
+    return result
         
 def countPoints(playerId, cards) -> None:
+    player1, player2 = 0, 0
     if playerId == 1:
-        for deck_id, card in enumerate(cards['cards']):
-            print(card['value'], card['suit'])
+        for card in enumerate(cards['cards']):
+            player1 += countCardWeigth(card["value"], card["suit"])
     elif playerId == 2:
-        for deck_id, card in enumerate(cards['cards']):
-            print(card['value'], card['suit'])
-
-def countCardWeigth(value, suit):
-
+        for card in enumerate(cards['cards']):
+            player2 += countCardWeigth(card["value"], card["suit"])
+    return player1, player2
 
 def startGame():
         # Перемешиваем колоду
     deck_id = api.shuffle_deck()
         # Вытаскиваем и печатаем 5 карт
     cards = api.draw_card(deck_id, count=0)
-
-    while cards['remaining']>=0:
-        countPoints(1, api.draw_card(deck_id, count=5))
-        countPoints(2, api.draw_card(deck_id, count=5))
-        cards
+    game1, game2 = 0, 0
+    while cards['remaining']>0:
+        game1 += countPoints(1, api.draw_card(deck_id, count=5))
+        game2 += countPoints(2, api.draw_card(deck_id, count=5))
+    
+    player_win = lambda x, y, str1, str2: str1 if x > y else str2
+    player_win(game1, game2, "player1", "player2")
 
 if __name__ == "__main__":
     startGame()
