7eed98e11c33fc67b825e7a8775ac8eaacc7159d
Assir Abdukhalikov <asirushtomblack@gmail.com>
2023-09-04 21:12:41 +0600

    lesson 12 end

--- lesson12-files/main.py
+++ lesson12-files/main.py
@@ -9,15 +9,37 @@ def task1(input: str, output: str):
     arr = []
 
     with open(input, "r") as f:
-        print(f.readlines())
-        for i in input:
-            if len(i) > 7:
-                arr.append(i)
-
-    with open(output + ".txt", "w", encoding='UTF-8') as f:
+        arr = f.readlines()
         for i in arr:
-            f.writeline(i)
+            if len(i) < 7:
+                arr.remove(i)
+
+    with open(output + ".txt", "w") as f:
+        f.writelines(arr)
         
+# task4
+def task4(input: str, output: str):
+    file_content = []
+    result = ""
+    valueexists = False
+    with open(input, "r") as rf:
+        file_content = rf.readlines()[::-1]
+    for i in file_content:
+        if i.__contains__(","):
+            result = i
+            valueexists=True
+    if(valueexists==False):
+        result = file_content[0]
+    with open(output, "w") as wf:
+        wf.write(result)
+    
+def task7(arr: list, file: str):
+    with open(file, "w") as wf:
+        while i in arr:
+            wf.write(i)
 
 if "__main__" == __name__:
-    task1("file1", "file2")
+    arr = [1,2,3]
+    #task1("file1", "file2")
+    #task4("file1.txt", "file2.txt")
+    task7(arr, "file2.txt")
